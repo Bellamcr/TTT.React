9ca13a7b9320a5d3b8d60ce17d2d26610fd689c6
Bellamcr <izabella.mcr@gmail.com>
2023-10-30 18:25:55 -0600

    Adding anothe button

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
 export default function Square() {
-  return <button className="square">X</button>;
+  return ( 
+  <>
+    <button className="square">X</button>
+    <button className="square">X</button>
+  </>
+  );
 }
